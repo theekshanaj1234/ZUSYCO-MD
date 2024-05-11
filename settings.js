ce2e4b80801bfc7bb09de247d08739104da8bcd5
theekshanaj1234 <167957084+theekshanaj1234@users.noreply.github.com>
2024-05-11 18:52:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -42,7 +42,7 @@ module.exports = {
   ANTI_LINKS : 'chat.whatsapp.com',
   ANTI_LINK : false,
   ANTI_LINK_KICK : false,
-  MAX_SIZE : 1024,
+  MAX_SIZE : 500,
   MOROCCO_NUMBERS : {
         BLOCK : false,
         NOT_WORKING : false,
